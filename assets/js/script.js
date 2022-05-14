30a149571371bf3ed63c268a343206f2bd06625f
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-05-14 22:56:10 +0530

    attacked Planes added

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(function (){
+
+    var playerPlane =$('#playerPlane');
+
+
+    var gameOver=false;
+
+
+
+
+
+
+    $(document).on('keydown', function (e) {
+        if (game_over === false) {
+            var key = e.keyCode;
+            if (key === 37 && move_left === false) {
+                move_left = requestAnimationFrame(left);
+            } else if (key === 39 && move_right === false) {
+                move_right = requestAnimationFrame(right);
+            } else if (key === 38 && move_up === false) {
+                move_up = requestAnimationFrame(up);
+            } else if (key === 40 && move_down === false) {
+                move_down = requestAnimationFrame(down);
+            }
+        }
+    });
+
+});
