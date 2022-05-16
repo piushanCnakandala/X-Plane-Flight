03d998a5abf1e33121a0ee52f9e70c17ae6837ec
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-05-16 11:51:48 +0530

    attacked plnes moved done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,12 @@ $(function (){
 
     var gameBoard = $('#gameBoard');
     var playerPlane = $('#playerPlane');
+    var attackedPlanes=$('#AttackedPlanes')
     var attackedPlane1 =$('#attackedPlane1');
     var attackedPlane2 =$('#attackedPlane2');
     var attackedPlane3 =$('#attackedPlane3');
     var attackedPlane4 =$('#attackedPlane4');
+    var gameLine =$('#gameLine')
 
     var game_over = false;
     var score_counter = 1;
@@ -65,6 +67,93 @@ $(function (){
             move_right = requestAnimationFrame(right);
         }
     }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////
+                   /*Attacked planes moved*/
+
+    anim_id = requestAnimationFrame(repeat);
+
+    function repeat() {
+        if (collision(gameLine, attackedPlane1) || collision(gameLine, attackedPlane2) || collision(gameLine, attackedPlane3) || collision(gameLine, attackedPlane4)) {
+            stop_the_game();
+            return;
+        }
+
+        score_counter++;
+
+        /*if (score_counter % 20 == 0) {
+            score.text(parseInt(score.text()) + 1);
+        }
+        if (score_counter % 500 == 0) {
+            speed++;
+            line_speed++;
+        }*/
+
+        attackedPlane_down(attackedPlane1);
+        attackedPlane_down(attackedPlane2);
+        attackedPlane_down(attackedPlane3);
+        attackedPlane_down(attackedPlane3);
+
+
+        anim_id = requestAnimationFrame(repeat);
+    }
+
+    function attackedPlane_down(AttackedPlane) {
+        var car_current_top = parseInt(AttackedPlane.css('top'));
+        if (car_current_top > gameBoard_height) {
+            car_current_top = -200;
+            var car_left = parseInt(Math.random() * (gameBoard_width - playerPlane_width));
+            AttackedPlane.css('left', car_left);
+        }
+       AttackedPlane.css('top', car_current_top + speed);
+    }
+
+
+  /*  restart_btn.click(function() {
+        location.reload();
+    });*/
+
+    function stop_the_game() {
+        game_over = true;
+        cancelAnimationFrame(anim_id);
+        cancelAnimationFrame(move_right);
+        cancelAnimationFrame(move_left);
+        cancelAnimationFrame(move_up);
+        cancelAnimationFrame(move_down);
+        restart_div.slideDown();
+        restart_btn.focus();
+        setHighScore();
+    }
+
+    /*function setHighScore() {
+        if (high_score < parseInt(score.text())) {
+            high_score = parseInt(score.text());
+            localStorage.setItem('high_score', parseInt(score.text()));
+        }
+        $('#high_score').text(high_score);
+    }*/
+
+
+    function collision($div1, $div2) {
+        var x1 = $div1.offset().left;
+        var y1 = $div1.offset().top;
+        var h1 = $div1.outerHeight(true);
+        var w1 = $div1.outerWidth(true);
+        var b1 = y1 + h1;
+        var r1 = x1 + w1;
+        var x2 = $div2.offset().left;
+        var y2 = $div2.offset().top;
+        var h2 = $div2.outerHeight(true);
+        var w2 = $div2.outerWidth(true);
+        var b2 = y2 + h2;
+        var r2 = x2 + w2;
+
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+        return true;
+    }
+
 
 });
 
